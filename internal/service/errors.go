07c836231e52405a69f9374aa177752fe46065ed
kkazantsevrv <ramzankazantsev@yandex.ru>
2025-11-20 18:53:43 +0500

    add errors

---
+++ internal/service/errors.go
@@ -0,0 +1,10 @@
+package service
+
+import "errors"
+
+var (
+	ErrNotFound     = errors.New("not found")
+	ErrConflict     = errors.New("already exists")
+	ErrInvalidInput = errors.New("invalid input")
+	ErrPrecondition = errors.New("precondition failed") // Например, статус merged
+)
