586db34bbaf206f60043f04c58a28602f784e796
kkazantsevrv <simplenamer1@gmail.com>
2025-11-24 16:32:03 +0500

    indexes issue

--- migrations/000002_add_indexes.down.sql
+++ migrations/000002_add_indexes.down.sql
@@ -1,11 +1,3 @@
--- 1. Ускоряет поиск кандидатов в команде:
--- SELECT ... FROM users WHERE team_name = $1 AND is_active = TRUE
-CREATE INDEX idx_users_team_active ON users(team_name, is_active);
-
--- 2. Ускоряет поиск PR конкретного автора:
--- SELECT ... FROM pull_requests WHERE author_id = $1
-CREATE INDEX idx_pull_requests_author ON pull_requests(author_id);
-
--- 3. Ускоряет получение списка ревью (GetUsersGetReview):
--- SELECT ... JOIN pr_reviewers ... WHERE reviewer_id = $1
-CREATE INDEX idx_pr_reviewers_reviewer ON pr_reviewers(reviewer_id);
+DROP INDEX IF EXISTS idx_pr_reviewers_reviewer;
+DROP INDEX IF EXISTS idx_pull_requests_author;
+DROP INDEX IF EXISTS idx_users_team_active;
